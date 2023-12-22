fe4f0dbf8abee7c203351c402ecab6dee379c63a
calvinrdtya <zeelink99@gmail.com>
2023-12-22 21:47:56 +0700

    update layout contentevent

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,7 +82,6 @@ const sr = ScrollReveal({
     reset: true,
 })
 
-
 sr.reveal(`.home__data, .home__social-link, .home__info,
            .discover__container,
            .experience__data, .experience__overlay,
@@ -105,26 +104,3 @@ sr.reveal(`.about__img-overlay,
     origin: 'right',
     interval: 100,
 })
-
-/*==================== DARK LIGHT THEME ====================*/ 
-// const themeButton = document.getElementById('theme-button')
-// const darkTheme = 'dark-theme'
-// const iconTheme = 'ri-sun-line'
-
-// const selectedTheme = localStorage.getItem('selected-theme')
-// const selectedIcon = localStorage.getItem('selected-icon')
-
-// const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-// const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
-
-// if (selectedTheme) {
-//   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-//   themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
-// }
-
-// themeButton.addEventListener('click', () => {
-//     document.body.classList.toggle(darkTheme)
-//     themeButton.classList.toggle(iconTheme)
-//     localStorage.setItem('selected-theme', getCurrentTheme())
-//     localStorage.setItem('selected-icon', getCurrentIcon())
-// })
